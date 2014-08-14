809badf355c3b5cfcfa1edbd01dcde2dec64d41a
Mallory Marcot <mmarcot@gmail.com>
2014-08-14 11:13:16 +0200

    Adding javadoc in AbstractGameState

--- src/game_state/AbstractGameState.java
+++ src/game_state/AbstractGameState.java
@@ -13,9 +13,41 @@ public abstract class AbstractGameState {
 	
 	protected GameStateManager gsm;
 	
+	/**
+	 * Initialize ressources of the state
+	 */
 	public abstract void init();
+	
+	/**
+	 * Updating the logical part of the state
+	 */
 	public abstract void update();
+	
+	/**
+	 * Rendering the state
+	 * @param g Graphical context
+	 */
 	public abstract void draw(Graphics2D g);
+	
+	/**
+	 * Defines events to do when key is pressed
+	 * @param key The Key pressed
+	 */
 	public abstract void keyPressed(KeyEvent key);
+	
+	/**
+	 * Defines events to do when key is released
+	 * @param key The Key released
+	 */
 	public abstract void keyReleased(KeyEvent key);
 }
+
+
+
+
+
+
+
+
+
+
